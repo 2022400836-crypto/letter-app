fa8f7615bc17d9db561084c43c92f6566aba5a12
2022400836-crypto <2022400836@pampangastateu.edu.ph>
2026-04-17 00:38:04 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -61,3 +61,6 @@ app.post("/letters", (req, res) => {
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
